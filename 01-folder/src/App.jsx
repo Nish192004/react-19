38ab033c0906e6400d9359d0951de19d1dda1173
Rajshekhar ojha <nishantojha70@gmail.com>
2025-11-20 12:48:43 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from './card.jsx';
+
+const App = () => {
+  return (
+    <div className="parent">
+     <Card user='Tony Stack'age='45'img="https://i.pinimg.com/736x/dd/6e/53/dd6e534ba212f85fb6d9da97224b849b.jpg"/>
+     <Card user='Tobey'age='21'img="https://images.unsplash.com/photo-1761706745195-26e5eef2fe3c?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+     <Card user='John Doe'age='23'img="https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg"/>
+     <Card user='Jane Smith'age='27'img="https://images.unsplash.com/photo-1762509347740-0586629cdfba?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0N3x8fGVufDB8fHx8fA%3D%3D"/>
+     <Card user='Alice Johnson'age='19'img="https://images.pexels.com/photos/1375849/pexels-photo-1375849.jpeg"/>
+     <Card user='Bob Brown'age='28'img="https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg"/>
+     <Card user='Charlie Davis'age='32'img="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+     <Card user='Eve Wilson'age='26'img="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"/>
+     <Card user='Frank Thomas'age='30'img="https://images.unsplash.com/photo-1527980965255-d3b416303d12?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+      <Card user='Grace Lee'age='24'img="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg"/>
+      <Card user='Hank Miller'age='29'img="https://images.pexels.com/photos/672444/pexels-photo-672444.jpeg"/>
+      <Card user='Ivy Martinez'age='22'img="https://images.pexels.com/photos/1376042/pexels-photo-1376042.jpeg"/>
+      <Card user='Jack Garcia'age='31'img="https://images.pexels.com/photos/1559486/pexels-photo-1559486.jpeg"/>
+      <Card user='Kathy Rodriguez'age='25'img="https://images.pexels.com/photos/1055691/pexels-photo-1055691.jpeg"/>
+      <Card user='Leo Hernandez'age='33'img="https://images.pexels.com/photos/264172/pexels-photo-264172.jpeg"/>
+      <Card user='Mia Lopez'age='20'img="https://images.pexels.com/photos/235462/pexels-photo-235462.jpeg"/>
+    
+    </div>
+  );
+};
+
+export default App;
